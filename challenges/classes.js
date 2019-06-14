9799361a9ec46dd928b7efd5403b7d0042b9b928
JasonYoo1 <jyoojs@gmail.com>
2019-06-14 09:25:31 -0700

    completed objects-arrays.js

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,6 +1,5 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
 
-test
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 // console.log(cuboid.volume()); // 100
